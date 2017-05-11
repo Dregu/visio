cdc79246c823ce5662871f77ac3cfaf99b073cb4
Dregu <dregu@dreg.us>
2017-05-11 21:11:40 +0300

    hmm

--- index.js
+++ index.js
@@ -4,117 +4,121 @@ const net = require('net')
 const dgram = require('dgram')
 const WSServer = require('ws').Server
 const Split = require('stream-split')
-const NALSeparator = new Buffer([0,0,0,1])
+const NALSeparator = new Buffer([0, 0, 0, 1])
 const express = require('express')
 const systemd = require('systemd')
 const app = express()
 
-var wsServer, conf = require('nconf'), headers = []
+var wsServer, conf = require('nconf'),
+  headers = []
 conf.argv().defaults({
-	tcpport: false,
-	udpport: false,
-	wsport: false,
-	queryport: false,
-	limit: 150
+  tcpport: 8000,
+  udpport: 8000,
+  wsport: 8081,
+  queryport: 8080,
+  limit: 150
 })
-if (conf.get('tcpport') && conf.get('udpport')) throw new Error('dont use tcp and udp together')
 
 if (conf.get('queryport')) {
-	app.get('/', (req, res) => {
-		var count = 0
-		wsServer.clients.forEach((ws) => {
-			if (ws.readyState == 1) {
-				count++
-			}
-		})
-		res.set('Content-type', 'text/plain')
-		res.send(count.toString())
-	})
-	app.listen(conf.get('queryport'))
+  app.get('/', (req, res) => {
+    var count = 0
+    wsServer.clients.forEach((ws) => {
+      if (ws.readyState == 1) {
+        count++
+      }
+    })
+    res.set('Content-type', 'text/plain')
+    res.send(count.toString())
+  })
+  app.listen(conf.get('queryport'))
 }
 
 function broadcast(data) {
-	wsServer.clients.forEach((ws) => {
-		if (ws.readyState === 1) {
-			ws.send(data, { binary: true })
-		}
-	})
+  wsServer.clients.forEach((ws) => {
+    if (ws.readyState === 1) {
+      ws.send(data, { binary: true })
+    }
+  })
 }
 
 if (conf.get('tcpport')) {
-	const tcpServer = net.createServer((socket) => {
-		console.log('streamer connected')
-		socket.on('end', () => {
-			console.log('streamer disconnected')
-		})
-		headers = []
-		const NALSplitter = new Split(NALSeparator)
-		NALSplitter.on('data', (data) => {
-			if (wsServer && wsServer.clients.length > 0) {
-				//data = Buffer.concat([NALSeparator, data])
-				if (headers.length < 3) headers.push(data)
-				broadcast(data)
-			}
-		}).on('error', (e) => {
-			console.log('splitter error '+e)
-			process.exit(0)
-		})
-		socket.pipe(NALSplitter)
-	})
-	tcpServer.listen(conf.get('tcpport'))
-	if (conf.get('tcpport') == 'systemd') {
-		console.log('TCP server listening on systemd socket')
-	} else {
-		var address = tcpServer.address()
-		if (address) console.log(`TCP server listening on ${address.address}:${address.port}`)
-	}
+  const tcpServer = net.createServer((socket) => {
+    console.log('streamer connected')
+    socket.on('end', () => {
+      console.log('streamer disconnected')
+    })
+    headers = []
+    const NALSplitter = new Split(NALSeparator)
+    NALSplitter.on('data', (data) => {
+      if (wsServer && wsServer.clients.length > 0) {
+        // why is this concat needed with (lib)x264 but not raspivid!?
+        data = Buffer.concat([NALSeparator, data])
+        if (headers.length < 3) headers.push(data)
+        broadcast(data)
+      }
+    }).on('error', (e) => {
+      console.log('splitter error ' + e)
+      process.exit(0)
+    })
+    socket.pipe(NALSplitter)
+  })
+  tcpServer.listen(conf.get('tcpport'))
+  if (conf.get('tcpport') == 'systemd') {
+    console.log('TCP server listening on systemd socket')
+  } else {
+    var address = tcpServer.address()
+    if (address) console.log(
+      `TCP server listening on ${address.address}:${address.port}`)
+  }
 }
 
 if (conf.get('udpport')) {
-	const udpServer = dgram.createSocket('udp4')
-	udpServer.on('listening', () => {
-		var address = udpServer.address()
-		console.log(`UDP server listening on ${address.address}:${address.port}`)
-	})
-	const NALSplitter = new Split(NALSeparator)
-	NALSplitter.on('data', (data) => {
-		if (wsServer && wsServer.clients.length > 0) {
-			//data = Buffer.concat([NALSeparator, data])
-			broadcast(data)
-		}
-	}).on('error', (e) => {
-		console.log('splitter error '+e)
-		process.exit(0)
-	})
-	udpServer.on('message', (msg, rinfo) => {
-		NALSplitter.write(msg)
-	})
-	udpServer.bind(conf.get('udpport'))
+  const udpServer = dgram.createSocket('udp4')
+  udpServer.on('listening', () => {
+    var address = udpServer.address()
+    console.log(
+      `UDP server listening on ${address.address}:${address.port}`)
+  })
+  const NALSplitter = new Split(NALSeparator)
+  NALSplitter.on('data', (data) => {
+    if (wsServer && wsServer.clients.length > 0) {
+      // this shit again...
+      data = Buffer.concat([NALSeparator, data])
+      broadcast(data)
+    }
+  }).on('error', (e) => {
+    console.log('splitter error ' + e)
+    process.exit(0)
+  })
+  udpServer.on('message', (msg, rinfo) => {
+    NALSplitter.write(msg)
+  })
+  udpServer.bind(conf.get('udpport'))
 }
 
 if (conf.get('wsport')) {
-	wsServer = new WSServer({ port: conf.get('wsport') })
-	console.log(`WS server listening on ${wsServer.options.host}:${wsServer.options.port}`)
-	wsServer.on('connection', (ws) => {
-		var currentClients = 0
-		wsServer.clients.forEach((ws) => {
-			if (ws.readyState === 1) {
-				currentClients++
-			}
-		})
-		if(currentClients > conf.get('limit')) {
-			console.log('client rejected, limit reached')
-			ws.close()
-			return
-		}
-		console.log('client connected, watching '+wsServer.clients.length)
-		for(let i in headers) {
-			ws.send(headers[i])
-		}
-		ws.on('close', (ws, id) => {
-			console.log('client disconnected, watching '+wsServer.clients.length)
-		})
-	})
+  wsServer = new WSServer({ port: conf.get('wsport') })
+  console.log(
+    `WS server listening on ${wsServer.options.host}:${wsServer.options.port}`
+  )
+  wsServer.on('connection', (ws) => {
+    var currentClients = 0
+    wsServer.clients.forEach((ws) => {
+      if (ws.readyState === 1) {
+        currentClients++
+      }
+    })
+    if (currentClients > conf.get('limit')) {
+      console.log('client rejected, limit reached')
+      ws.close()
+      return
+    }
+    console.log('client connected, watching ' + wsServer.clients.length)
+    for (let i in headers) {
+      ws.send(headers[i])
+    }
+    ws.on('close', (ws, id) => {
+      console.log('client disconnected, watching ' + wsServer.clients.length)
+    })
+  })
 }
-
-
--- raspi.sh
+++ raspi.sh
@@ -2,24 +2,23 @@
 
 # UDP mode, with extra options for my halogen lighting. Don't worry about it...
 #raspivid -w 848 -h 480 -t 0 -fps 25 -ih -b 700000 -pf baseline -mm average -ISO 800 -awb off -awbg 1.0,2.5 -ex fixedfps -ev 0 -co 50 -br 65 -o - |\
-raspivid -w 960 -h 544 -t 0 -fps 25 -ih -g 50 -b 1000000 -pf baseline -mm average -ISO 400 -awb off -awbg 1.0,2.5 -ex fixedfps -ev 0 -co 50 -br 58 -roi 0,0,1,1 -o - |\
- nc stream.wappuradio.fi 15000
- #socat - tcp:stream.wappuradio.fi:15000,shut-none
-
-#  gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=1 ! udpsink host=stream.wappuradio.fi port=7000
-#  tee >(socat - tcp:stream.wappuradio.fi:9000,shut-none,reuseaddr) |
-
-
-#  tee >(socat - tcp:stream.wappuradio.fi:7000,shut-none,reuseaddr) |\
-#  socat - tcp:stream.wappuradio.fi:9000,shut-none,reuseaddr
+#socat - udp-sendto:localhost:8000,shut-none
 
 # TCP mode, minimal setup
 #raspivid -w 848 -h 480 -t 0 -fps 25 -ih -b 700000 -pf baseline -o - | nc localhost 8000
-
-# We can do 1080p30 if we really want
-#raspivid -w 1920 -h 1080 -t 0 -fps 30 -g 60 -ih -b 1200000 -pf baseline -o - | nc localhost 8000
 
 # GStreamer kinda works, but the OMX encoder doesn't use inline headers.
 # Our server however saves the SPS/PPS headers in TCP mode and sends them to every new client.
 #modprobe bcm2835-v4l2
 #gst-launch-1.0 v4l2src ! video/x-raw,width=848,height=480,framerate=25/1 ! omxh264enc control-rate=2 target-bitrate=700000 ! video/x-h264,width=848,height=480,framerate=25/1,stream-format=byte-stream,profile=baseline ! tcpclientsink host=localhost port=8000
+
+# Test stream for swarm mode
+# You can duplicate the actual raspivid stream on the server with socat and tee
+
+#gst-launch-1.0 videotestsrc ! video/x-raw,width=848,height=480,framerate=25/1 ! x264enc bitrate=700 ! video/x-h264,width=848,height=480,framerate=25/1,stream-format=byte-stream,profile=baseline ! tee name=t\
+# t. ! queue ! tcpclientsink host=172.18.0.3 port=8000 \
+# t. ! queue ! tcpclientsink host=172.18.0.4 port=8000 \
+# t. ! queue ! tcpclientsink host=172.18.0.5 port=8000 \
+# t. ! queue ! tcpclientsink host=172.18.0.6 port=8000
+
+# TODO socat and tee example
